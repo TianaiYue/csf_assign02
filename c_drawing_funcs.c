ad7f9593eb31ba44513c79b8416313e27d8bf08e
Cassie Zhang <xiyanzhang9@gmail.com>
2024-02-09 23:12:11 -0500

    implemented all recommended helper functions

--- c_drawing_funcs.c
+++ c_drawing_funcs.c
@@ -64,34 +64,48 @@ uint8_t get_a(uint32_t color) {
 // blends foreground and background color component values 
 // using a specified alpha (opacity) value
 uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha) {
-
+  uint32_t result = (alpha * fg + (255 - alpha) * bg) / 255;
+  return (uint8_t)result;
 }
 
 // blends foreground and background colors using the 
 // foreground color’s alpha value to produce an opaque color. 
 // (It will call blend_components.)
 uint32_t blend_colors(uint32_t fg, uint32_t bg) {
+  uint8_t fg_a = get_a(fg);
+  uint8_t blended_r = blend_components(get_r(fg), get_r(bg), fg_a);
+  uint8_t blended_g = blend_components(get_g(fg), get_g(bg), fg_a);
+  uint8_t blended_b = blend_components(get_b(fg), get_b(bg), fg_a);
 
+  uint32_t blended_color = ((uint32_t)blended_r << 24) | ((uint32_t)blended_g << 16) | ((uint32_t)blended_b << 8) | ((uint32_t)255);
+    
+  return blended_color;
 }
 
 // draws a single pixel to a destination image, blending the 
 // specified foregroudn color with the existing background 
 // color, at a specified pixel index
 void set_pixel(struct Image *img, uint32_t index, uint32_t color) {
-
+  if (index < img->width * img->height) {
+    uint32_t bg_color = img->data[index];
+    uint32_t blended_color = blend_colors(color, bg_color);
+    img->data[index] = blended_color;
+  }
 }
 
-// returns the result of squaring an int64_t value. The square_dist 
-// function returns the sum of the squares of the x and y distances 
-// between two points.
+// returns the result of squaring an int64_t value.
 int64_t square(int64_t x) {
-
+  return x * x;
 }
 
 // returns the sum of the squares of the x and y distances 
 // between two points
 int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
+  int64_t distance_x = x2 - x1;
+  int64_t distance_y = y2 - y1;
 
+  // Return the sum of the squares of the differences
+  return square(distance_x) + square(distance_y);
 }
 
 
