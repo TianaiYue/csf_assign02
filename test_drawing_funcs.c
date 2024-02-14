c297e98063b0ad901bf4c0a41902db7c61b5cfc6
Cassie Zhang <xiyanzhang9@gmail.com>
2024-02-13 20:13:45 -0500

    added function prototypes

--- test_drawing_funcs.c
+++ test_drawing_funcs.c
@@ -14,6 +14,20 @@
 #include "drawing_funcs.h"
 #include "tctest.h"
 
+// add prototypes for your helper functions
+int32_t in_bounds(struct Image *img, int32_t x, int32_t y); 
+uint32_t compute_index(struct Image *img, int32_t x, int32_t y);
+int32_t clamp(int32_t val, int32_t min, int32_t max);
+uint8_t get_r(uint32_t color);
+uint8_t get_g(uint32_t color);
+uint8_t get_b(uint32_t color);
+uint8_t get_a(uint32_t color);
+uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha);
+uint32_t blend_colors(uint32_t fg, uint32_t bg);
+void set_pixel(struct Image *img, uint32_t index, uint32_t color);
+int64_t square(int64_t x);
+int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
+
 // an expected color identified by a (non-zero) character code
 typedef struct {
   char c;
@@ -114,7 +128,7 @@ int main(int argc, char **argv) {
 
   TEST_INIT();
 
-  // TODO: add TEST() directives for your helper functions
+  // add TEST() directives for your helper functions
   TEST(test_draw_pixel);
   TEST(test_draw_rect);
   TEST(test_draw_circle);
