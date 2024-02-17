5d4c985be2963146be2634f52bb52e293cf4e09a
Cassie Zhang <xiyanzhang9@gmail.com>
2024-02-17 14:24:49 -0500

    fix

--- test_drawing_funcs.c
+++ test_drawing_funcs.c
@@ -17,16 +17,16 @@
 // add prototypes for your helper functions
 int32_t in_bounds(struct Image *img, int32_t x, int32_t y); 
 uint32_t compute_index(struct Image *img, int32_t x, int32_t y);
-// int32_t clamp(int32_t val, int32_t min, int32_t max);
-// uint8_t get_r(uint32_t color);
-// uint8_t get_g(uint32_t color);
-// uint8_t get_b(uint32_t color);
-// uint8_t get_a(uint32_t color);
+int32_t clamp(int32_t val, int32_t min, int32_t max);
+uint8_t get_r(uint32_t color);
+uint8_t get_g(uint32_t color);
+uint8_t get_b(uint32_t color);
+uint8_t get_a(uint32_t color);
 uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha);
-// uint32_t blend_colors(uint32_t fg, uint32_t bg);
-// void set_pixel(struct Image *img, uint32_t index, uint32_t color);
-// int64_t square(int64_t x);
-// int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
+uint32_t blend_colors(uint32_t fg, uint32_t bg);
+void set_pixel(struct Image *img, uint32_t index, uint32_t color);
+int64_t square(int64_t x);
+int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
 
 // an expected color identified by a (non-zero) character code
 typedef struct {
@@ -98,27 +98,27 @@ void check_picture(struct Image *img, Picture *p) {
 }
 
 // prototypes of test functions
-// void test_draw_pixel(TestObjs *objs);
-// void test_draw_rect(TestObjs *objs);
-// void test_draw_circle(TestObjs *objs);
-// void test_draw_circle_clip(TestObjs *objs);
-// void test_draw_tile(TestObjs *objs);
-// void test_draw_sprite(TestObjs *objs);
+void test_draw_pixel(TestObjs *objs);
+void test_draw_rect(TestObjs *objs);
+void test_draw_circle(TestObjs *objs);
+void test_draw_circle_clip(TestObjs *objs);
+void test_draw_tile(TestObjs *objs);
+void test_draw_sprite(TestObjs *objs);
 
 void test_in_bounds(TestObjs *objs);
 void test_compute_index(TestObjs *objs);
 void test_blend_components(TestObjs *objs);
-// void test_set_pixel(TestObjs *objs);
-// void test_set_pixel_2(TestObjs *objs);
-// void test_square(TestObjs *objs);
+void test_set_pixel(TestObjs *objs);
+void test_set_pixel_2(TestObjs *objs);
+void test_square(TestObjs *objs);
 
-// void test_clamp(TestObjs *objs);
-// void test_get_r(TestObjs *objs);
-// void test_get_g(TestObjs *objs);
-// void test_get_b(TestObjs *objs);
-// void test_get_a(TestObjs *objs);
-// void test_blend_colors(TestObjs *objs);
-// void test_square_dist(TestObjs *objs);
+void test_clamp(TestObjs *objs);
+void test_get_r(TestObjs *objs);
+void test_get_g(TestObjs *objs);
+void test_get_b(TestObjs *objs);
+void test_get_a(TestObjs *objs);
+void test_blend_colors(TestObjs *objs);
+void test_square_dist(TestObjs *objs);
 
 int main(int argc, char **argv) {
   if (argc > 1) {
