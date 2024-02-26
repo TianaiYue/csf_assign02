01b3d8ea41a4f0780f92099b56edb49c68e4497d
Tianai Yue'git config --global user.name Tianai <tyue4@jh.edu>
2024-02-26 00:33:27 -0500

    rect

--- test_drawing_funcs.c
+++ test_drawing_funcs.c
@@ -130,7 +130,7 @@ int main(int argc, char **argv) {
 
   // add TEST() directives for your helper functions
   TEST(test_draw_pixel);
-  // TEST(test_draw_rect);
+  TEST(test_draw_rect);
   TEST(test_draw_circle);
   TEST(test_draw_circle_clip);
   // TEST(test_draw_tile);
--- test_drawing_funcs_secret.c
+++ test_drawing_funcs_secret.c
@@ -117,9 +117,9 @@ int main(int argc, char **argv) {
   // TODO: add TEST() directives for your helper functions
   TEST(test_draw_pixel);
 #if 1
-  // TEST(test_draw_rect);
-  // TEST(test_draw_circle);
-  // TEST(test_draw_circle_clip);
+  TEST(test_draw_rect);
+  TEST(test_draw_circle);
+  TEST(test_draw_circle_clip);
   // TEST(test_draw_tile);
   // TEST(test_draw_sprite);
 #endif
@@ -134,8 +134,8 @@ int main(int argc, char **argv) {
   TEST(test_draw_pixel_secret_2);
   // TEST(test_draw_rect_secret_1);
   // TEST(test_draw_rect_secret_2);
-  // TEST(test_draw_circle_secret_1);
-  // TEST(test_draw_circle_secret_2);
+  TEST(test_draw_circle_secret_1);
+  TEST(test_draw_circle_secret_2);
   // TEST(test_draw_tile_secret_1);
   // TEST(test_draw_tile_secret_2);
   // TEST(test_draw_sprite_secret_1);
